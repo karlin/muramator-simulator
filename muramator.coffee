86f25939fc80adf3a5c35a1926dbeb840196c603
karlin <karlin@atomicobject.com>
2013-03-11 18:54:58 -0500

    much improved

--- muramator.coffee
+++ muramator.coffee
@@ -1,62 +1,20 @@
+# see also, http://jsfiddle.net/karlin/dQvQg/1/
+
 $ ->
   v=0
-  clock = (n) =>
-    =>
-      n.active = (v++) % 2 == 0
-      v
-
-  onAllTheTime = -> 1
 
   neurons = [
-      name: 'detector'
-      x: 100
-      y: 100
-    ,
-      name: 'detectObstacle'
-      cycle: 15
-      x: 200
-      y: 100
-    ,
-      name: 'S1'
-      x: 300
-      y: 100
-    ,
-      name: 'S2'
-      x: 350
-      y: 250
-    ,
-      name: 'turn'
-      x: 400
-      y: 100
-    ,
-      name: 'forward'
-      x: 300
-      y: 300
-    ,
-      name: 'explore_ex'
-      allTheTime: true
-      x: 200
-      y: 200
-    ,
-      name: 'seek'
-      cycle: 5000
-      x: 150
-      y: 150
-    ,
-      name: 'seek_ex'
-      allTheTime: true
-      x: 100
-      y: 280
-    ,
-      name: 'emit_ex'
-      allTheTime: true
-      x: 50
-      y: 100
-    ,
-      name: 'emitter'
-      cycle: 6
-      x: 50
-      y: 200
+    {name:'detector',       x:100, y:100},
+    {name:'detectObstacle', x:200, y:100, cycle:15},
+    {name:'S1',             x:300, y:100},
+    {name:'S2',             x:350, y:250},
+    {name:'turn',           x:400, y:100},
+    {name:'forward',        x:300, y:300},
+    {name:'explore_ex',     x:200, y:200, allTheTime:true},
+    {name:'seek',           x:150, y:150, cycle:5000},
+    {name:'seek_ex',        x:100, y:280, allTheTime: true},
+    {name:'emit_ex',        x:50,  y:100, allTheTime: true},
+    {name:'emitter',        x:50,  y:200, cycle:6},
   ]
 
   N = (x) => (i for i in neurons when i.name is x)[0]
@@ -118,143 +76,143 @@ $ ->
       weight: -2
   ]
 
-  for n in neurons
-    n.active = true
-  #   if n.allTheTime then
-  #     n.fn = -> 1
-  #   else
-  #     n.fn = -> n.output = n.value + 
+  neuronGraph = (nodes, links) ->
+    for n in nodes
+      n.active = true
+    #   if n.allTheTime then
+    #     n.fn = -> 1
+    #   else
+    #     n.fn = -> n.output = n.value + 
 
-  #
-  # Graph these puppies
-  #
+    #
+    # Graph these puppies
+    #
 
-  nodes = neurons
-  links = dendrites
+    w = 500
+    h = 350
+    r = 20
+    markerPath = "M 4.73,-6.26 5.08,-1.43 7.05,3.47 0,0z"
 
-  w = 500
-  h = 350
-  r = 20
-  markerPath = "M 4.73,-6.26 5.08,-1.43 7.05,3.47 0,0z"
+    svg = d3.select("body").append("svg")
+        .attr("width", w)
+        .attr("height", h);
 
-  svg = d3.select("body").append("svg")
-      .attr("width", w)
-      .attr("height", h);
+    svg.append("defs").append("marker")
+        .attr("id", "inh")
+        .attr("viewBox", "0 -7 10 10")
+        .attr("refX", 21.6)
+        .attr("refY", -3)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
+        .attr("orient", "auto")
+      .append("path")
+        .attr("d", markerPath)
+        
+    svg.append("defs").append("marker")
+        .attr("id", "exc")
+        .attr("viewBox", "0 -7 10 10")
+        .attr("refX", 21.6)
+        .attr("refY", -3)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
+        .attr("orient", "auto")
+      .append("path")
+        .attr("d", markerPath)
 
-  svg.append("defs").append("marker")
-      .attr("id", "inh")
-      .attr("viewBox", "0 -7 10 10")
-      .attr("refX", 21.6)
-      .attr("refY", -3)
-      .attr("markerWidth", 8)
-      .attr("markerHeight", 8)
-      .attr("orient", "auto")
-    .append("path")
-      .attr("d", markerPath)
-      
-  svg.append("defs").append("marker")
-      .attr("id", "exc")
-      .attr("viewBox", "0 -7 10 10")
-      .attr("refX", 21.6)
-      .attr("refY", -3)
-      .attr("markerWidth", 8)
-      .attr("markerHeight", 8)
-      .attr("orient", "auto")
-    .append("path")
-      .attr("d", markerPath)
+    force = d3.layout.force()
+      .nodes(nodes)
+      .links(links)
+      .size([w, h])
+      .linkDistance((d) -> if d.size? then d.size else r*5)
+      .charge(-r*12)
+      .start()
 
-  force = d3.layout.force()
-    .nodes(nodes)
-    .links(links)
-    .size([w, h])
-    .linkDistance((d) -> if d.size? then d.size else r*5)
-    .charge(-r*12)
-    .start()
+    text = svg.selectAll("text.weight-label")
+        .data(links)
+      .enter().append("text")
+        .attr("class", "weight-label")
 
-  text = svg.selectAll("text.weight-label")
-      .data(links)
-    .enter().append("text")
-      .attr("class", "weight-label")
+    linkClass = (l) =>
+      "link #{if l.weight < 0 then "inh" else "exc"}"
 
-  linkClass = (l) =>
-    "link #{if l.weight < 0 then "inh" else "exc"}"
+    linkMarker = (l) =>
+      "url(##{if l.weight < 0 then "inh" else "exc"})"
 
-  linkMarker = (l) =>
-    "url(##{if l.weight < 0 then "inh" else "exc"})"
+    path = svg.append("g").selectAll("path")
+        .data(links)
+      .enter().append("path")
+        .attr("class", "link")
+        .attr("marker-end", linkMarker)
 
-  path = svg.append("g").selectAll("path")
-      .data(links)
-    .enter().append("path")
-      .attr("class", "link")
-      .attr("marker-end", linkMarker)
+    node = svg.selectAll(".node")
+        .data(nodes)
+      .enter().append("g")
+        .attr("class", "node")
+        .attr("cx", (d) -> d.x)
+        .attr("cy", (d) -> d.y)
+      .call(force.drag)
 
-  node = svg.selectAll(".node")
-      .data(nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .attr("cx", (d) -> d.x)
-      .attr("cy", (d) -> d.y)
-    .call(force.drag)
+    nodeSize = (n) =>
+      if n.allTheTime then (r * 0.5) else r
 
-  nodeSize = (n) =>
-    if n.allTheTime then (r * 0.5) else r
+    nodeLabel = (n) =>
+      if n.allTheTime then '  *' else n.name
 
-  nodeLabel = (n) =>
-    if n.allTheTime then '  *' else n.name
+    circle = node.append("circle")
+        .attr("r", nodeSize)
+        .attr("fill", (d) -> if d.active then 'white' else 'black')
+    circle.append('title')
+        .text((n) -> n.name)
 
-  node.append("circle")
-      .attr("r", nodeSize)
-      .attr("fill", (n) -> 
-        return if n.active then "rgba(255,205,190,0.9)" else "rgba(155,105,250,1)"
-      )
+    node.append("text")
+        .attr("dy", ".2em")
+        .attr("text-anchor", "middle")
+        .attr("class", "node-label shadow")
+      .text(nodeLabel)
 
-  node.append("text")
-      .attr("dy", ".2em")
-      .attr("text-anchor", "middle")
-      .attr("class", "node-label shadow")
-    .text(nodeLabel)
+    node.append("text")
+        .attr("dy", ".2em")
+        .attr("text-anchor", "middle")
+        .attr("class", "node-label")
+      .text(nodeLabel).append('tspan')
+                .text((n)=> if n.cycle? then "#{n.cycle}ms" else '')
+                .attr("text-anchor", "middle")
+                .attr('dy', '1.1em')
+                .attr('x', '0px')
+                .attr('class', 'cycle-label')
 
-  node.append("text")
-      .attr("dy", ".2em")
-      .attr("text-anchor", "middle")
-      .attr("class", "node-label")
-    .text(nodeLabel).append('tspan')
-              .text((n)=> if n.cycle? then "#{n.cycle}ms" else '')
-              .attr("text-anchor", "middle")
-              .attr('dy', '1.1em')
-              .attr('x', '0px')
-              .attr('class', 'cycle-label')
-    
-  text.attr("x", (d) -> (d.source.x + d.target.x) / 2)
-      .attr("y", (d) -> (d.source.y + d.target.y) / 2)
-      .attr("text-anchor", "middle")
-    .text (t) -> "#{t.weight}"
-      
-  force.on "tick", ->
-    path.attr "d", (d) ->
-      dx = d.target.x - d.source.x
-      dy = d.target.y - d.source.y
-      dr = Math.sqrt(dx * dx + dy * dy)
-      "M#{d.source.x},#{d.source.y}A#{dr},#{dr} 0 0,1 #{d.target.x},#{d.target.y}"
-    
     text.attr("x", (d) -> (d.source.x + d.target.x) / 2)
         .attr("y", (d) -> (d.source.y + d.target.y) / 2)
-    
-    node.attr("transform", (d) -> "translate(#{d.x},#{d.y})")
+        .attr("text-anchor", "middle")
+      .text (t) -> "#{t.weight}"
+        
+    force.on "tick", ->
+      path.attr "d", (d) ->
+        dx = d.target.x - d.source.x
+        dy = d.target.y - d.source.y
+        dr = Math.sqrt(dx * dx + dy * dy)
+        "M#{d.source.x},#{d.source.y}A#{dr},#{dr} 0 0,1 #{d.target.x},#{d.target.y}"
+      
+      text.attr("x", (d) -> (d.source.x + d.target.x) / 2)
+          .attr("y", (d) -> (d.source.y + d.target.y) / 2)
+      
+      node.attr("transform", (d) -> "translate(#{d.x},#{d.y})")
+
+    circle
 
   random = d3.random.normal(1, 0.5)
 
-  weeGraph = (v) ->
+  weeGraph = (v, nodes) ->
     tick = ->
       data.push v()
       graph
-        .attr("d", line)
-        .attr("transform", null)
+          .attr("d", line)
+          .attr("transform", null)
         .transition()
-        .duration(500)
-        .ease("linear")
-        .attr("transform", "translate(" + x(0) + ")")
-        .each("end", tick)
+          .duration(500)
+          .ease("linear")
+          .attr("transform", "translate(" + x(0) + ")")
+          .each("end", tick)
       data.shift()
 
     n = 40
@@ -268,15 +226,13 @@ $ ->
     width = 160 - margin.left - margin.right
     height = 50 - margin.top - margin.bottom
     x = d3.scale.linear().domain([ 1, n - 2 ]).range([ 0, width ])
-    y = d3.scale.linear().domain([ 0, 2 ]).range([ height, 3 ])
+    y = d3.scale.linear().domain([ 0, 2 ]).range([ height, 3])
     line = d3.svg.line().interpolate("step-before").x((d, i) ->x(i)).y((d, i) -> y(d))
-    svg = d3.select("body").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    svg = d3.select("svg")
       .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
 
-    svg.append("defs").append("clipPath")
+    svg.select("defs").append("clipPath")
         .attr("id", "clip")
       .append("rect")
         .attr("width", width)
@@ -289,4 +245,19 @@ $ ->
         .attr("d", line)
     tick
 
-  weeGraph(clock(N('explore_ex')))()
+  updateNode = neuronGraph(neurons, dendrites)
+  d3.timer(->
+    updateNode.attr('class', (d) -> if d.active then "active" else "inactive")
+    false
+  ,200)
+  clock = (n) ->
+    ->
+      v += 1
+      v = v % 2
+      n.active = (v == 1)
+      v
+
+  onAllTheTime = -> 1
+  graphTick = weeGraph(clock(N('explore_ex')))
+  graphTick()
+
--- muramator.js
+++ muramator.js
@@ -2,18 +2,9 @@
 (function() {
 
   $(function() {
-    var N, clock, dendrites, force, h, kf, kt, linkClass, linkMarker, links, markerPath, n, neurons, node, nodeLabel, nodeSize, nodes, onAllTheTime, path, r, random, svg, text, v, w, weeGraph, _i, _len,
+    var N, clock, dendrites, graphTick, kf, kt, neuronGraph, neurons, onAllTheTime, random, updateNode, v, weeGraph,
       _this = this;
     v = 0;
-    clock = function(n) {
-      return function() {
-        n.active = (v++) % 2 === 0;
-        return v;
-      };
-    };
-    onAllTheTime = function() {
-      return 1;
-    };
     neurons = [
       {
         name: 'detector',
@@ -21,9 +12,9 @@
         y: 100
       }, {
         name: 'detectObstacle',
-        cycle: 15,
         x: 200,
-        y: 100
+        y: 100,
+        cycle: 15
       }, {
         name: 'S1',
         x: 300,
@@ -42,29 +33,29 @@
         y: 300
       }, {
         name: 'explore_ex',
-        allTheTime: true,
         x: 200,
-        y: 200
+        y: 200,
+        allTheTime: true
       }, {
         name: 'seek',
-        cycle: 5000,
         x: 150,
-        y: 150
+        y: 150,
+        cycle: 5000
       }, {
         name: 'seek_ex',
-        allTheTime: true,
         x: 100,
-        y: 280
+        y: 280,
+        allTheTime: true
       }, {
         name: 'emit_ex',
-        allTheTime: true,
         x: 50,
-        y: 100
+        y: 100,
+        allTheTime: true
       }, {
         name: 'emitter',
-        cycle: 6,
         x: 50,
-        y: 200
+        y: 200,
+        cycle: 6
       }
     ];
     N = function(x) {
@@ -138,95 +129,101 @@
         weight: -2
       }
     ];
-    for (_i = 0, _len = neurons.length; _i < _len; _i++) {
-      n = neurons[_i];
-      n.active = true;
-    }
-    nodes = neurons;
-    links = dendrites;
-    w = 500;
-    h = 350;
-    r = 20;
-    markerPath = "M 4.73,-6.26 5.08,-1.43 7.05,3.47 0,0z";
-    svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
-    svg.append("defs").append("marker").attr("id", "inh").attr("viewBox", "0 -7 10 10").attr("refX", 21.6).attr("refY", -3).attr("markerWidth", 8).attr("markerHeight", 8).attr("orient", "auto").append("path").attr("d", markerPath);
-    svg.append("defs").append("marker").attr("id", "exc").attr("viewBox", "0 -7 10 10").attr("refX", 21.6).attr("refY", -3).attr("markerWidth", 8).attr("markerHeight", 8).attr("orient", "auto").append("path").attr("d", markerPath);
-    force = d3.layout.force().nodes(nodes).links(links).size([w, h]).linkDistance(function(d) {
-      if (d.size != null) {
-        return d.size;
-      } else {
-        return r * 5;
+    neuronGraph = function(nodes, links) {
+      var circle, force, h, linkClass, linkMarker, markerPath, n, node, nodeLabel, nodeSize, path, r, svg, text, w, _i, _len,
+        _this = this;
+      for (_i = 0, _len = nodes.length; _i < _len; _i++) {
+        n = nodes[_i];
+        n.active = true;
       }
-    }).charge(-r * 12).start();
-    text = svg.selectAll("text.weight-label").data(links).enter().append("text").attr("class", "weight-label");
-    linkClass = function(l) {
-      return "link " + (l.weight < 0 ? "inh" : "exc");
-    };
-    linkMarker = function(l) {
-      return "url(#" + (l.weight < 0 ? "inh" : "exc") + ")";
-    };
-    path = svg.append("g").selectAll("path").data(links).enter().append("path").attr("class", "link").attr("marker-end", linkMarker);
-    node = svg.selectAll(".node").data(nodes).enter().append("g").attr("class", "node").attr("cx", function(d) {
-      return d.x;
-    }).attr("cy", function(d) {
-      return d.y;
-    }).call(force.drag);
-    nodeSize = function(n) {
-      if (n.allTheTime) {
-        return r * 0.5;
-      } else {
-        return r;
-      }
-    };
-    nodeLabel = function(n) {
-      if (n.allTheTime) {
-        return '  *';
-      } else {
-        return n.name;
-      }
-    };
-    node.append("circle").attr("r", nodeSize).attr("fill", function(n) {
-      if (n.active) {
-        return "rgba(255,205,190,0.9)";
-      } else {
-        return "rgba(155,105,250,1)";
-      }
-    });
-    node.append("text").attr("dy", ".2em").attr("text-anchor", "middle").attr("class", "node-label shadow").text(nodeLabel);
-    node.append("text").attr("dy", ".2em").attr("text-anchor", "middle").attr("class", "node-label").text(nodeLabel).append('tspan').text(function(n) {
-      if (n.cycle != null) {
-        return "" + n.cycle + "ms";
-      } else {
-        return '';
-      }
-    }).attr("text-anchor", "middle").attr('dy', '1.1em').attr('x', '0px').attr('class', 'cycle-label');
-    text.attr("x", function(d) {
-      return (d.source.x + d.target.x) / 2;
-    }).attr("y", function(d) {
-      return (d.source.y + d.target.y) / 2;
-    }).attr("text-anchor", "middle").text(function(t) {
-      return "" + t.weight;
-    });
-    force.on("tick", function() {
-      path.attr("d", function(d) {
-        var dr, dx, dy;
-        dx = d.target.x - d.source.x;
-        dy = d.target.y - d.source.y;
-        dr = Math.sqrt(dx * dx + dy * dy);
-        return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+      w = 500;
+      h = 350;
+      r = 20;
+      markerPath = "M 4.73,-6.26 5.08,-1.43 7.05,3.47 0,0z";
+      svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+      svg.append("defs").append("marker").attr("id", "inh").attr("viewBox", "0 -7 10 10").attr("refX", 21.6).attr("refY", -3).attr("markerWidth", 8).attr("markerHeight", 8).attr("orient", "auto").append("path").attr("d", markerPath);
+      svg.append("defs").append("marker").attr("id", "exc").attr("viewBox", "0 -7 10 10").attr("refX", 21.6).attr("refY", -3).attr("markerWidth", 8).attr("markerHeight", 8).attr("orient", "auto").append("path").attr("d", markerPath);
+      force = d3.layout.force().nodes(nodes).links(links).size([w, h]).linkDistance(function(d) {
+        if (d.size != null) {
+          return d.size;
+        } else {
+          return r * 5;
+        }
+      }).charge(-r * 12).start();
+      text = svg.selectAll("text.weight-label").data(links).enter().append("text").attr("class", "weight-label");
+      linkClass = function(l) {
+        return "link " + (l.weight < 0 ? "inh" : "exc");
+      };
+      linkMarker = function(l) {
+        return "url(#" + (l.weight < 0 ? "inh" : "exc") + ")";
+      };
+      path = svg.append("g").selectAll("path").data(links).enter().append("path").attr("class", "link").attr("marker-end", linkMarker);
+      node = svg.selectAll(".node").data(nodes).enter().append("g").attr("class", "node").attr("cx", function(d) {
+        return d.x;
+      }).attr("cy", function(d) {
+        return d.y;
+      }).call(force.drag);
+      nodeSize = function(n) {
+        if (n.allTheTime) {
+          return r * 0.5;
+        } else {
+          return r;
+        }
+      };
+      nodeLabel = function(n) {
+        if (n.allTheTime) {
+          return '  *';
+        } else {
+          return n.name;
+        }
+      };
+      circle = node.append("circle").attr("r", nodeSize).attr("fill", function(d) {
+        if (d.active) {
+          return 'white';
+        } else {
+          return 'black';
+        }
       });
+      circle.append('title').text(function(n) {
+        return n.name;
+      });
+      node.append("text").attr("dy", ".2em").attr("text-anchor", "middle").attr("class", "node-label shadow").text(nodeLabel);
+      node.append("text").attr("dy", ".2em").attr("text-anchor", "middle").attr("class", "node-label").text(nodeLabel).append('tspan').text(function(n) {
+        if (n.cycle != null) {
+          return "" + n.cycle + "ms";
+        } else {
+          return '';
+        }
+      }).attr("text-anchor", "middle").attr('dy', '1.1em').attr('x', '0px').attr('class', 'cycle-label');
       text.attr("x", function(d) {
         return (d.source.x + d.target.x) / 2;
       }).attr("y", function(d) {
         return (d.source.y + d.target.y) / 2;
+      }).attr("text-anchor", "middle").text(function(t) {
+        return "" + t.weight;
       });
-      return node.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
+      force.on("tick", function() {
+        path.attr("d", function(d) {
+          var dr, dx, dy;
+          dx = d.target.x - d.source.x;
+          dy = d.target.y - d.source.y;
+          dr = Math.sqrt(dx * dx + dy * dy);
+          return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+        });
+        text.attr("x", function(d) {
+          return (d.source.x + d.target.x) / 2;
+        }).attr("y", function(d) {
+          return (d.source.y + d.target.y) / 2;
+        });
+        return node.attr("transform", function(d) {
+          return "translate(" + d.x + "," + d.y + ")";
+        });
       });
-    });
+      return circle;
+    };
     random = d3.random.normal(1, 0.5);
-    weeGraph = function(v) {
-      var data, graph, height, line, margin, tick, width, x, y;
+    weeGraph = function(v, nodes) {
+      var data, graph, height, line, margin, n, svg, tick, width, x, y;
       tick = function() {
         data.push(v());
         graph.attr("d", line).attr("transform", null).transition().duration(500).ease("linear").attr("transform", "translate(" + x(0) + ")").each("end", tick);
@@ -251,12 +248,35 @@
       }).y(function(d, i) {
         return y(d);
       });
-      svg = d3.select("body").append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom).append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-      svg.append("defs").append("clipPath").attr("id", "clip").append("rect").attr("width", width).attr("height", height);
+      svg = d3.select("svg").append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      svg.select("defs").append("clipPath").attr("id", "clip").append("rect").attr("width", width).attr("height", height);
       graph = svg.append("g").attr("clip-path", "url(#clip)").append("path").data([data]).attr("class", "graph").attr("d", line);
       return tick;
     };
-    return weeGraph(clock(N('explore_ex')))();
+    updateNode = neuronGraph(neurons, dendrites);
+    d3.timer(function() {
+      updateNode.attr('class', function(d) {
+        if (d.active) {
+          return "active";
+        } else {
+          return "inactive";
+        }
+      });
+      return false;
+    }, 200);
+    clock = function(n) {
+      return function() {
+        v += 1;
+        v = v % 2;
+        n.active = v === 1;
+        return v;
+      };
+    };
+    onAllTheTime = function() {
+      return 1;
+    };
+    graphTick = weeGraph(clock(N('explore_ex')));
+    return graphTick();
   });
 
 }).call(this);
